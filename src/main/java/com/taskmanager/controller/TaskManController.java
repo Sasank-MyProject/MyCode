9afdc06d56d450be96882e1b2efeb8d8c6ab6a24
sasank <52821498+SasankNalam@users.noreply.github.com>
2019-07-12 17:02:31 +0530

    Update TaskManController.java

--- src/main/java/com/taskmanager/controller/TaskManController.java
+++ src/main/java/com/taskmanager/controller/TaskManController.java
@@ -1,6 +1,5 @@
 package com.taskmanager.controller;
 
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -21,26 +20,41 @@ public class TaskManController {
 
 	@Autowired
 	private TaskManRepository taskmanrepository;
-	
-	
 		
-	//get all the date into list 
+	/*
+	 * Get List of Task
+	 */ 
 	@RequestMapping(value = "/tasks", method= RequestMethod.GET)
 	public List<Task> getListOfPost(){
 		List<Task> posts = (List<Task>) taskmanrepository.findAll();			
 		return posts;
 	}
 	
+	/*
+	 * POST Method which accept Task object 
+	 * Persist Task object into database.
+	 * return Task object with Id.  
+	 */
 	@RequestMapping(value = "/task", method = RequestMethod.POST)
 	public Task savePost(@RequestBody Task tasks) {				
 		return taskmanrepository.save(tasks);			
 	}
 	
+	/*
+	 * GET  Method which accept id 
+	 * get Task object from database using Id.
+	 * return Task object.  
+	 */
 	@RequestMapping(value="/task/{id}",method = RequestMethod.GET)
 	public Task getPostById(@PathVariable(value="id") Long id){		
 		return taskmanrepository.findOne(id);
 	}
 	
+	/*
+	 * DELETE Method which accept id 
+	 * delete Task object from database using Id.
+	 * return Success Message.  
+	 */
 	@RequestMapping(value="/task/{id}", method = RequestMethod.DELETE)
 	public Map<String, String> deletePostById(@PathVariable Long id) {
 		Map<String, String> message = new HashMap<String,String>();
@@ -49,7 +63,11 @@ public class TaskManController {
 		return message;
 	}
 	
-	
+	/*
+	 * PUT Method which accept id and Task object.  
+	 * update Task object into database.
+     * return Updated Task object.  
+	 */
 	 @RequestMapping(value="/task/{id}", method=RequestMethod.PUT)
 	  public Task updatePost(@PathVariable Long id, @RequestBody Task tasks) {
 		Task update = taskmanrepository.findOne(id);
